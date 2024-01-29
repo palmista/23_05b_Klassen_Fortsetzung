6f524fd2961b26424a0548d8adf5f80eea31187c
Chris Weber <chris.weber@kslzh.ch>
2024-01-29 14:31:45 +0100

    first commit

---
+++ Fraction.java
@@ -0,0 +1,35 @@
+public class Fraction {
+    static int numberOfFractions;
+    int numerator, denominator;
+    
+    // public Fraction(Fraction f) {
+    //     this(f.numerator, f.denominator);
+    // }
+
+    // public Fraction() {
+    //     this(0, 1);
+    // }
+
+    // public void setDenominator(int denominator) {
+    //     if (denominator == 0) {
+    //         throw new ArithmeticException("Division by zero");
+    //     } else {
+    //         this.denominator = denominator;
+    //     }
+    // }
+
+    // public void add(Fraction f) {
+    //     this.setNumerator(this.numerator * f.denominator + this.denominator * f.numerator);
+    //     this.setDenominator(this.denominator * f.denominator);
+    // }
+
+    // public static Fraction add(Fraction f, Fraction g) {
+    //     f.add(g);
+    //     return f;
+    // }
+    
+    // public boolean equals(Fraction f) {
+    //     return this.numerator * f.denominator == this.denominator * f.numerator;
+    // }
+
+}
---
+++ FractionTester.java
@@ -0,0 +1,21 @@
+public class FractionTester {
+    public static void main(String[] args) {
+        Fraction f = new Fraction();
+        // Der folgende Code funktioniert nur, wenn die Felder von Fraction nicht privat sind:
+        f.numerator = 12;
+        f.denominator = 0;
+        System.out.println(f.numerator + "/" + f.denominator);
+        
+        // toString():
+
+        // Encapsulation, Getter und Setter:
+
+        // Copy-Konstruktor und equals():
+        
+        // Klassen- und Instanzenmethoden:
+
+        // statische Variablen:
+
+    }
+
+}
